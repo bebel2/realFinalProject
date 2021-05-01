282f769a6422b0be355a3c39dcb933a525b4adf1
bebel2 <bebel2@huskers.unl.edu>
2021-05-01 18:39:31 -0500

    Update finalProject.m

--- finalProject.m
+++ finalProject.m
@@ -3,12 +3,14 @@ function [] = finalProject()
     global R;
     R = 8.314;
  gui.fig = figure('numbertitle', 'off', 'name', 'Van Der Waals Equation'); 
- gui.temperature = uicontrol('style','slider');
- gui.temperatureLabel = uicontrol('style','text','string','slide to your temperature in Kelvin');
- gui.criticalTemperature = uicontrol('style','edit');
- gui.criticalPressure = uicontrol('style','edit');
- gui.specificVolume = uicontrol('style','edit');
-    %edit
+ gui.temperature = uicontrol('style','edit','units', 'normalized', 'position', [.05 .04 .10 .05]);
+ gui.temperatureLabel = uicontrol('style','text','string','temperature in K','units', 'normalized', 'position', [.05 .14 .10 .10]);
+ gui.criticalTemperature = uicontrol('style','edit','units', 'normalized', 'position', [.15 .04 .10 .05]);
+ gui.criticalTemperatureLabel = uicontrol('style','text','string',' critical temperature in K','units', 'normalized', 'position', [.15 .14 .10 .10]);
+ gui.criticalPressure = uicontrol('style','edit','units', 'normalized', 'position', [.25 .04 .10 .05]);
+ gui.criticalPressureLabel =uicontrol('style','text','string','critical pressure in Pa','units', 'normalized', 'position', [.25 .14 .10 .10]);
+ gui.specificVolume = uicontrol('style','edit','units', 'normalized', 'position', [.35 .04 .10 .05]);
+ gui.specificVolumeLabel = uicontrol('style','text','string','volume in m^3/mol','units', 'normalized', 'position', [.35 .14 .10 .10]);
 
 end
 function [] = calculateAVariable()
@@ -17,12 +19,12 @@ end
 function [] = calculateBVariable()
 
 end
-function [] = calculatetermOne()
+function [] = calculateTermOne()
 
 end
-function [] = calculatetermTwo()
+function [] = calculateTermTwo()
 
 end
 function [] = calculatePressure()
-P = termOne - termTwo
+P = calculateTermOne - calculateTermTwo;
 end
