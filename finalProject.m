a1a35211ebdceafdf363542de115934560a1df35
bebel2 <bebel2@huskers.unl.edu>
2021-05-03 12:34:46 -0500

    Update finalProject.m

--- finalProject.m
+++ finalProject.m
@@ -12,9 +12,9 @@ function [] = finalProject()
  gui.criticalPressureLabel =uicontrol('style','text','string','critical pressure in Pa','units', 'normalized', 'position', [.25 .14 .10 .10]);
  gui.specificVolume = uicontrol('style','edit','units', 'normalized', 'position', [.35 .04 .10 .05]);
  gui.specificVolumeLabel = uicontrol('style','text','string','volume in m^3/mol','units', 'normalized', 'position', [.35 .14 .10 .10]);
- gui.startButton = uicontrol('style', 'pushbutton', 'units', 'normalized', 'string', 'calculate pressure', 'position', [.55 .14 .20 .05]);
+ gui.startButton = uicontrol('style', 'pushbutton', 'units', 'normalized', 'string', 'calculate pressure', 'position', [.55 .14 .20 .05],'callback', {@calculatePressure});
 end
-function [] = calculatePressure(source,event)
+function [P] = calculatePressure(source,event)
 global gui
 gui.source = gui;
 P = calculateTermOne - calculateTermTwo;
