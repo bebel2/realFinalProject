9a3235f4f2c9586878c8e229fcd3e14fe4d1dafe
bebel2 <82912895+bebel2@users.noreply.github.com>
2021-05-03 19:41:56 -0500

    Delete mySum.m

--- mySum.m
+++
@@ -1,9 +0,0 @@
-    function [] = mySum(n)  
-        i = 0; 
-        sum = n;
-        while i < n  
-            sum = sum + i;  
-            i = i + 1; 
-        end
-        fprintf('The sum of 0:%d = %d\n',n,sum);
-    end
